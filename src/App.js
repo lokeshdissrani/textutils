4a1599c86bc2229dd69363a1fc9c1ad127893069
lokeshdissrani <lokeshdissrani@gmail.com>
2023-07-10 12:57:32 +0530

    Checking

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
@@ -9,6 +10,7 @@ function App() {
     <Navbar title="TextUtils" aboutText="About TextUtils" />
     <div className="container my-3">
       <TextForm heading="Enter your text to analyze below" />
+      {/* <About/> */}
     </div>
   </>
   );
